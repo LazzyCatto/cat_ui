8f74476f1194dd233961400b76ca4b3702d86f2e
Akio <lazzycatto@yandex.com>
2025-07-18 17:18:00 +0300

    add ButtonText and solve isue with SelectorLabel

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cat_ui",
-    version="1.1.0",
+    version="1.1.1",
     description="Simple python ui lib for cli programs",
     author="Akio Smiowly",
     author_email="lazzycatto@yandex.com",
